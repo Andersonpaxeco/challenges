b9dfad5be069322b7f5e718c2031e82518778901
Andersonpaxeco <138760793+Andersonpaxeco@users.noreply.github.com>
2023-12-16 19:45:03 +0000

    Check the source folder and destination folder change

--- replic.py
+++ replic.py
@@ -8,10 +8,14 @@ from datetime import datetime
 
 def synchronize_folders(source_folder, replica_folder):
     # Make sure source and replica folders exist
-    if not os.path.exists(source_folder) or not os.path.exists(replica_folder):
-        os.mkdir(replica_folder)
-        #print("Source or replica folder does not exist.")
+    if not os.path.exists(source_folder):
+        print("Source folder does not exist.")
         return
+
+    if not os.path.exists(replica_folder):
+            os.mkdir(replica_folder)
+            #print("Source or replica folder does not exist.")
+            #return
 
     # Get the list of files in the source folder
     source_files = set(os.listdir(source_folder))
@@ -24,16 +28,23 @@ def synchronize_folders(source_folder, replica_folder):
 
     # Files to be removed from replica folder
     to_remove = replica_files - source_files
+    curr_datetime = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
 
-    log_file = "sync_log_hour.txt"
+    print (curr_datetime)
+    log_file = (r"c:\temp\sync_log" + curr_datetime + ".txt")
     logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(message)s')
 
 
     # Copy new or modified files
     for file_name in to_copy:
+        
         source_path = os.path.join(source_folder, file_name)
         replica_path = os.path.join(replica_folder, file_name)
+        print (source_path)
+        print (replica_path)
         shutil.copy2(source_path, replica_path)
+       
+        #shutil.copy2(source_folder+file_name,replica_folder+file_name)
         print(f"Copied: {file_name}")
 
     # Remove files not present in the source folder
